8182a0f2259a6a6b730e3e3b3c67131424e6cd45
JackieLiu <liutong6053@163.com>
2016-11-09 15:51:41 +0800

    修改灵云和机器翻译服务.

--- src/main/java/com/ai/yc/protal/web/controller/YeekitController.java
+++ src/main/java/com/ai/yc/protal/web/controller/YeekitController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,13 +20,23 @@ import com.ai.yc.protal.web.exception.HttpStatusException;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
+/**
+ * 译库机器翻译
+ */
 @Controller
 public class YeekitController {
     private static final Logger LOGGER = LoggerFactory.getLogger(YeekitController.class);
-    public static final String SERVER_URL = "http://api.yeekit.com/dotranslate.php";
-    public static final String TRANSLAN_URL = "http://192.168.52.3:9006/detection";
-    public static final String APP_KID = "58105e00cabc3";
-    public static final String APP_KEY = "53eeb0bb6c1b613ab361a4f8057b2bd9";
+
+    @Value("${yeekit.translate.url}")
+    private String SERVER_URL;
+
+    //语言检测
+    @Value("${yee.detection.url}")
+    private String TRANSLAN_URL;
+    @Value("${yeekit.translate.appkid}")
+    private String APP_KID;
+    @Value("${yeekit.translate.appkey}")
+    private String APP_KEY;
     
    
 
--- src/main/java/com/ai/yc/protal/web/service/HcicloudService.java
+++ src/main/java/com/ai/yc/protal/web/service/HcicloudService.java
@@ -32,7 +32,7 @@ public class HcicloudService {
     @Value("${hcicloud.url}")
     private String SERVER_URL;
     private static final  byte[] WAVHEAD8K = {82, 73, 70, 70, 2, 70, 0, 0, 87, 65, 86, 69, 102, 109, 116, 32, 16, 0, 0, 0, 1, 0, 1, 0, 64, 31, 0, 0, -128, 62, 0, 0, 2, 0, 16, 0, 100, 97, 116, 97, -34, 69, 0, 0};
-//    @Autowired
+    @Autowired
     private CloseableHttpClient client;
 
     /**
