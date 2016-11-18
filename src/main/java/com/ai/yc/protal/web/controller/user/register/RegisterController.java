893677f432f7a169fb5fade3608963cb62fcab1f
xuyw <xuyw@xuyw-PC>
2016-11-18 16:51:57 +0800

    修改获取ip

--- src/main/java/com/ai/yc/protal/web/controller/user/register/RegisterController.java
+++ src/main/java/com/ai/yc/protal/web/controller/user/register/RegisterController.java
@@ -286,7 +286,20 @@ public class RegisterController {
 		if (!StringUtil.isBlank(password)) {
 			req.setPassword(Md5Utils.md5(password));
 		}
-		req.setRegip("0");
+		req.setRegip(getIpAddr(request));
 		return req;
+	}
+	private String getIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
 	}
 }
