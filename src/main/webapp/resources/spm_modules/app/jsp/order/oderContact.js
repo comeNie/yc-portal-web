6e1f81bafcdb2dc2d1d76d602c8e10a452477fef
mimw <mimw@asiainfo.com>
2016-12-05 18:17:48 +0800

    译员订单信息 译文信息修改

--- src/main/webapp/resources/spm_modules/app/jsp/order/oderContact.js
+++ src/main/webapp/resources/spm_modules/app/jsp/order/oderContact.js
@@ -126,7 +126,7 @@ define('app/jsp/order/oderContact', function (require, exports, module) {
                         //成功
                         $("#saveContactDiv").hide();
                         $("#editContactDiv").find('p').eq(0).html($("#userName").val());
-                        $("#editContactDiv").find('p').eq(1).html("+"+$("#globalRome").find('option:selected').attr('code')+" "+$("#mobilePhone").val());
+                        $("#editContactDiv").find('p').eq(1).html($("#globalRome").find('option:selected').attr('code')+$("#mobilePhone").val());
                         $("#editContactDiv").find('p').eq(2).html($("#email").val());
                         $("#editContactDiv").show();
                     }
