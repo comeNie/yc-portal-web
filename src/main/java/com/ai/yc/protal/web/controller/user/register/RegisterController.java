be6d79ed963f09ed9c40b908b5b1ffd6fa6926e5
houbin <1031248990@qq.com>
2016-12-19 10:55:43 +0800

    fix gain several ip address from X-Forwarded-For

--- src/main/java/com/ai/yc/protal/web/controller/user/register/RegisterController.java
+++ src/main/java/com/ai/yc/protal/web/controller/user/register/RegisterController.java
@@ -329,17 +329,45 @@ public class RegisterController {
 		req.setRegip(getIpAddr(request));
 		return req;
 	}
+	
 	private String getIpAddr(HttpServletRequest request) {
-		String ip = request.getHeader("x-forwarded-for");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("Proxy-Client-IP");
+		try{
+			String remoteAddr = request.getHeader("X-Forwarded-For");
+			// 如果通过多级反向代理，X-Forwarded-For的值不止一个，而是一串用逗号分隔的IP值，此时取X-Forwarded-For中第一个非unknown的有效IP字符串
+			if (isEffective(remoteAddr) && (remoteAddr.indexOf(",") > -1)) {
+				String[] array = remoteAddr.split(",");
+				for (String element : array) {
+					if (isEffective(element)) {
+						remoteAddr = element;
+						break;
+					}
+				}
+			}
+			if (!isEffective(remoteAddr)) {
+				remoteAddr = request.getHeader("X-Real-IP");
+			}
+			if (!isEffective(remoteAddr)) {
+				remoteAddr = request.getRemoteAddr();
+			}
+			return remoteAddr;
+		}catch(Exception e){
+			LOG.error("get romote ip error,error message:"+e.getMessage());
+			return "";
 		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("WL-Proxy-Client-IP");
+	}
+	/**
+	 * 远程地址是否有效.
+	 * 
+	 * @param remoteAddr
+	 *            远程地址
+	 * @return true代表远程地址有效，false代表远程地址无效
+	 */
+	private static boolean isEffective(final String remoteAddr) {
+		boolean isEffective = false;
+		if ((null != remoteAddr) && (!"".equals(remoteAddr.trim()))
+				&& (!"unknown".equalsIgnoreCase(remoteAddr.trim()))) {
+			isEffective = true;
 		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getRemoteAddr();
-		}
-		return ip;
+		return isEffective;
 	}
 }
