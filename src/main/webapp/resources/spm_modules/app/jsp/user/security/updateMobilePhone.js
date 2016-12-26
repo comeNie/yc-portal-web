e7437e8ca41c0f0716ea4ca15be35be2f2e84dfd
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-12-26 18:42:10 +0800

    调整页面

--- src/main/webapp/resources/spm_modules/app/jsp/user/security/updateMobilePhone.js
+++ src/main/webapp/resources/spm_modules/app/jsp/user/security/updateMobilePhone.js
@@ -254,7 +254,7 @@ define("app/jsp/user/security/updateMobilePhone",
 												var step = 59;
 												_dynamicode_btn.val(updatePhoneJs.resend60);
 									            var _res = setInterval(function(){
-									            	_dynamicode_btn.val(step + ' s');
+									            	_dynamicode_btn.val(step+"S"+updatePhoneJs.resend);
 									                step-=1;
 									                if(step < 0){
 									                _dynamicode_btn.removeAttr("disabled"); //移除disabled属性
